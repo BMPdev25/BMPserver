2c89e02ca46eb4125bd3e7481158de3e3ed545da
Anish Grandhe <grandheanish@gmail.com>
2025-09-20 01:34:14 +0530

    changed server

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(compression());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cors({
-  origin: process.env.CLIENT_URL || true,
+  origin: '*',
   credentials: true,
 }));
 
